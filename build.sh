f73b90f84cf51e99d0dc581f6b535e6a4d98e3b5
Tim Düsterhus <timwolla@arcor.de>
2011-09-08 22:47:59 +0200

    Corrected templates filename

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 # Building WCFSetup.tar.gz
 
-cd ./com.woltlab.wcf/templates/
-tar -cf ../templates.tar *
+cd ./com.woltlab.wcf/template/
+tar -cf ../template.tar *
 cd ../
-tar -cf ../wcfsetup/install/packages/com.woltlab.wcf.tar * --exclude=templates
-rm templates.tar
+tar -cf ../wcfsetup/install/packages/com.woltlab.wcf.tar * --exclude=template
+rm template.tar
 cd ../wcfsetup
 tar -czf ../WCFSetup.tar.gz install setup
 rm install/packages/com.woltlab.wcf.tar
